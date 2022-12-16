e188d4797e7a81d2135defd036ebac80e22285d8
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2022-12-16 00:50:26 -0500

    create methods `apiGetMovieById` and `apiGetRatings`

--- backend/api/MoviesController.js
+++ backend/api/MoviesController.js
@@ -25,4 +25,30 @@ export default class MoviesController {
         };
         res.json(response);
     }
+
+    static async apiGetMovieById(req, res, next) {
+        try {
+            const id = req.params.id || {};
+            const movie = await MoviesDAO.getMovieById(id);
+
+            if (!movie) {
+                res.status(404).json({ error: 'not found' });
+                return;
+            }
+            res.json(movie);
+        } catch (e) {
+            console.log(`api, ${e}`);
+            res.status(500).json({ error: e });
+        }
+    }
+
+    static async apiGetRatings(req, res, next) {
+        try {
+            const propertyTypes = await MoviesDAO.getRatings();
+            res.json(propertyTypes);
+        } catch (e) {
+            console.log(`api,${e}`);
+            res.status(500).json({ error: e });
+        }
+    }
 }
