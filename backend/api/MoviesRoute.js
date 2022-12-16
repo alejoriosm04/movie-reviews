32c02c444ba999df7975fdb1d60d0c2b7add5257
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2022-12-16 00:36:02 -0500

    add the two `routes` to show specific movies
    - These two routes retrieves a specific movie associated with an specific condition.
      - The first to get movies with an specific `id` and the second one to get movies with an specific `rating`

--- backend/api/MoviesRoute.js
+++ backend/api/MoviesRoute.js
@@ -4,6 +4,8 @@ import ReviewsController from './ReviewsController.js';
 export default class MoviesRoute {
     static configRoutes(router) {
         router.route('/').get(MoviesController.apiGetMovies);
+        router.route('/id/:id').get(MoviesController.apiGetMoviesById);
+        router.route('/ratings').get(MoviesController.apiGetRatings);
 
         router
             .route('/review')
