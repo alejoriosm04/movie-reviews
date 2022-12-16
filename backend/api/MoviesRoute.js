65f073e13efbe673bf335efe0036736607603727
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2022-12-16 01:21:14 -0500

    fix `apiGetMovieById` route

--- backend/api/MoviesRoute.js
+++ backend/api/MoviesRoute.js
@@ -4,7 +4,7 @@ import ReviewsController from './ReviewsController.js';
 export default class MoviesRoute {
     static configRoutes(router) {
         router.route('/').get(MoviesController.apiGetMovies);
-        router.route('/id/:id').get(MoviesController.apiGetMoviesById);
+        router.route('/id/:id').get(MoviesController.apiGetMovieById);
         router.route('/ratings').get(MoviesController.apiGetRatings);
 
         router
