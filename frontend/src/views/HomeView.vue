f261601f546827bf8eea03a15e546570ce109ff2
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2022-12-18 01:13:45 -0500

    add `Bootstrap` framework
    - The UI of the website was changed using a template of Bootstrap
    - Added in `HomeView.vue` the `bootstrap.min.css` and the `bootstrap.min.js`

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="card">
+      <div class="card-header">
+        Movies
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Movies Application</h5>
+        <p class="card-text">Welcome to the Movies Application</p>
+        <img src="https://images.pexels.com/photos/3137890/pexels-photo-3137890.jpeg" class="img-fluid" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
+  name: 'Home',
 };
 </script>
