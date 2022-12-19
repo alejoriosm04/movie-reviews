89ab35dc27cb92b8f091d2e41159b653266c5b3e
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2022-12-18 20:32:43 -0500

    add `Login.vue` component

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            Login
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">This is the login</h5>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+};
+</script>
