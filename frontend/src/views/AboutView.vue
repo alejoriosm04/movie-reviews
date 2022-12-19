8f7a99b1c95e78b8c4d2ba35c934f571cab50317
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2022-12-18 20:21:33 -0500

    add `About View` as one of the headers

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="card">
+    <div class="card-header">
+      About
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">This is an about page</h5>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+};
+</script>
