96e8b33faabf67d836ef69526d8875fa33665154
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2022-12-18 23:41:53 -0500

    create Review Service
    - Same funcionalities and structure as in Movies Service using `Axios`

---
+++ frontend/src/services/ReviewService.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export default class ReviewService {
+    static async createReview(data) {
+        const res = await axios.post(
+            `http://localhost:5000/api/v1/movies/review`,
+            data
+        );
+        return res;
+    }
+
+    static async deleteReview(data) {
+        const res = await axios.delete(
+            `http://localhost:5000/api/v1/movies/review`,
+            { data }
+        );
+        return res;
+    }
+
+    static async updateReview(data) {
+        const res = await axios.put(
+            `http://localhost:5000/api/v1/movies/review`,
+            data
+        );
+        return res;
+    }
+}
