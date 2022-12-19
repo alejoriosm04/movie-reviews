0f00ab0db8190e219b6392ac71af15b8807cfc85
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2022-12-18 21:35:34 -0500

    create main `footer` section in `App.vue` root component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -31,6 +31,17 @@
     <div class="container mt-4">
       <router-view/>
     </div>
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-light text-muted mt-4">
+      <div class="text-center p-4" style="background-color: gainsboro;">
+        © 2023 Copyright -
+        <a class="text-reset fw-bold text-decoration-none"
+          target="_blank" href="https://twitter.com/alejoriosm04"
+        >
+          Alejandro Rios
+        </a>
+      </div>
+    </footer>
   </div>
 </template>
 
