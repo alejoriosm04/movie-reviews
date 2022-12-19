26c72a10cbfe31cc890c4f7a929bb3e4d0482cad
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2022-12-18 20:26:18 -0500

    add `Movies.vue` component

---
+++ frontend/src/views/Movies.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            Movie Results
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">This is the movie results</h5>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Movies',
+};
+</script>
