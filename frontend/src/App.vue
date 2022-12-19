26f4b5effe74e866ee7254564c87be986bafaab6
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2022-12-18 20:41:23 -0500

    create main `header` in `App.vue` root components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,44 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <!-- Header -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+        <router-link to="/" class="navbar-brand">Movies APP</router-link>
+        <button class="navbar-toggler" type="button"
+          data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup" aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <router-link to="/" class="nav-link active">Home</router-link>
+            <router-link to="/movies" class="nav-link active">
+              Movies
+            </router-link>
+            <router-link to="/about" class="nav-link active">
+              About
+            </router-link>
+              <router-link to="/login" class="nav-link active">
+                Login
+              </router-link>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- Header -->
+    <div class="container mt-4">
+      <router-view/>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
 
 <style>
 #app {
@@ -13,18 +47,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
