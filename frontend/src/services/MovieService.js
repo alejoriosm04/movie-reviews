87ed2d30f00be7c7649e10c49301ef0120a0ed05
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2022-12-18 23:32:07 -0500

    create Movie Service
    - The services that a search for Movies should return were connected between frontend and backend
    - Created functions which make the API calls to the backend endpoints

---
+++ frontend/src/services/MovieService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export default class MovieService {
+    static async getMovies(query = '', by = 'title', page = 0) {
+        const res = await axios.get(
+            `http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}`
+        );
+        return res.data;
+    }
+
+    static async getMovie(id) {
+        const res = await axios.get(
+            `http://localhost:5000/api/v1/movies/id/${id}`
+        );
+        return res.data;
+    }
+
+    static async getRatings() {
+        const res = await axios.get(
+            `http://localhost:5000/api/v1/movies/ratings`
+        );
+        return res.data;
+    }
+}
