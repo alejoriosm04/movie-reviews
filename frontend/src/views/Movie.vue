2967ad83d9265521d5a97f55190856c2ffeb3670
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2022-12-21 01:16:28 -0500

    add `Listing Reviews` view in `Movie` view

--- frontend/src/views/Movie.vue
+++ frontend/src/views/Movie.vue
@@ -16,6 +16,15 @@
                     </div>
                     <hr />
                     <h3>Reviews</h3>
+                    <ul class="list-group">
+                        <li class="list-group-item pb-3 pt-3" v-for="review in movie.reviews" :key="review._id">
+                            <h5 class="card-title">Review by {{ review.name }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ review.date }}</h6>
+                            <p class="card-text">{{ review.review }}</p>
+                            <a class="btn btn-primary me-3"> Edit </a>
+                            <a class="btn btn-danger"> Delete </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
